2f8f54ae31f796dc963c8bc7eab10d039608a3a9
adityaprasad97 <prasadaditya97@gmail.com>
2020-06-10 20:45:33 +0530

    working

--- database.js
+++ database.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
 					port: process.env.DB_PORT || 5432,
 					dialectOptions: {
 						ssl: {
-							process.env.DB_SSL == "true",
+							require: true,
 							rejectUnauthorized: false
 						}
 
